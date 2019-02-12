bbca4b3cffeabfa34c433dd0d7be60789af1b36f
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-12 12:11:23 +0100

    created many-to-many association within user model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,8 @@ class UserController < ApplicationController
   def my_portfolio
       @user_stocks = UserStock.where user_id: current_user.id
   end
+  def friendship
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'user#my_portfolio'
+  get 'friendship', to: 'user#friendship'
   get 'search', to: 'market#search'
   resources :user_stock, only: [:create, :destroy]
 end
